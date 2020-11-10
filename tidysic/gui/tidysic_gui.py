b487e1399b2a6bc67d58f32846fd9f68a0a32c26
g-wizzy <pierre.burki@protonmail.com>
2020-11-10 14:32:14 +0100

    Fix linter

--- tidysic/gui/tidysic_gui.py
+++ tidysic/gui/tidysic_gui.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets
 
-from tidysic import Tag, log, create_structure
+from tidysic import Tag, create_structure
 
 from .folder_select_dialog import FolderSelectDialog
 
@@ -16,7 +16,7 @@ def run():
         input_dir = input_dialog.selectedFiles()[0]
         ordering = [Tag.Artist, Tag.Album]
 
-        artists = create_structure(input_dir, ordering, False, True)
+        artists = create_structure(input_dir, ordering, False, True)  # noqa
 
     window.show()
     app.exec()
