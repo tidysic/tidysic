9bbeee863d18dfaf357ba17c48d1193a2be2ac8d
g-wizzy <pierre.burki@protonmail.com>
2020-11-10 14:20:27 +0100

    Fix __init__.py

--- tidysic/__init__.py
+++ tidysic/__init__.py
@@ -1,2 +1,25 @@
-from .algorithms import (parse_in_directory,  # noqa
-    move_files, clean_up, organize)  # noqa
+from .algorithms import (
+    move_files,
+    clean_up,
+    organize
+)
+from .audio_file import AudioFile
+from .tag import Tag
+from .logger import (
+    log,
+    warning,
+    error,
+    dry_run
+)
+
+__all__ = [
+    move_files,
+    clean_up,
+    organize,
+    AudioFile,
+    Tag,
+    log,
+    warning,
+    error,
+    dry_run
+]
