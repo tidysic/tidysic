b3aac869d90263e0ba78108dbeb01d88524415c6
g-wizzy <pierre.burki@protonmail.com>
2020-11-14 18:57:15 +0100

    Workaround for guess test

--- test/test_algorithm.py
+++ test/test_algorithm.py
@@ -3,6 +3,7 @@ import os
 
 from tidysic.os_utils import project_test_folder, get_audio_files
 from tidysic.tag import Tag
+from tidysic.audio_file import AudioFile
 from tidysic.algorithms import create_structure
 
 
@@ -33,19 +34,19 @@ class AlgorithmTest(TestCase):
         self.assertEqual(len(tree.ordered.keys()), 1)
 
         for artist_name, artist_subtree in tree.ordered.items():
-        self.assertEqual(artist_name, 'L\'Artiste')
-        self.assertEqual(len(artist_subtree.unordered), 0)
-        self.assertIsInstance(artist_subtree.ordered, dict)
-        self.assertEqual(len(artist_subtree.ordered.keys()), 1)
+            self.assertEqual(artist_name, 'L\'Artiste')
+            self.assertEqual(len(artist_subtree.unordered), 0)
+            self.assertIsInstance(artist_subtree.ordered, dict)
+            self.assertEqual(len(artist_subtree.ordered.keys()), 1)
 
             for album_name, album_subtree in artist_subtree.ordered.items():
-        self.assertEqual(album_name, 'L\'Album')
-        self.assertEqual(len(album_subtree.unordered), 0)
-        self.assertIsInstance(album_subtree.ordered, list)
-        self.assertEqual(len(album_subtree.ordered), 1)
+                self.assertEqual(album_name, 'L\'Album')
+                self.assertEqual(len(album_subtree.unordered), 0)
+                self.assertIsInstance(album_subtree.ordered, list)
+                self.assertEqual(len(album_subtree.ordered), 1)
 
-        song = album_subtree.ordered[0]
-        self.assertListEqual(files, [song])
+                song = album_subtree.ordered[0]
+                self.assertListEqual(files, [song])
 
         format = '{title}'
         self.assertEqual(song.build_file_name(format), 'Le Titre')
@@ -57,6 +58,8 @@ class AlgorithmTest(TestCase):
             'Missing Artist - No Title'
         )
         files = get_audio_files(path)
+
+        AudioFile.accept_all_guesses = True  # Needed in order to test without user input
 
         tree = create_structure(
             files,
