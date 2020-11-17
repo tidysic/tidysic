6969b761a0ef758924b97da65d6803080452b5ab
g-wizzy <pierre.burki@protonmail.com>
2020-11-17 20:27:09 +0100

    Setup minimal main window

--- tidysic/gui/main_window.py
+++ tidysic/gui/main_window.py
@@ -1,12 +1,9 @@
-from PyQt5.QtWidgets import (
-    QMainWindow,
-    QHBoxLayout
-)
+from PyQt5.QtWidgets import QMainWindow
 
-from tidysic.gui.widgets import (
-    OrderingLevelSelect,
-    TreeEditor
-)
+from tidysic.os_utils import get_audio_files
+from tidysic.algorithms import create_structure
+
+from tidysic.gui.widgets import TreeEditor
 from tidysic.gui.dialogs import (
     FolderSelect,
     OrderingSelect
@@ -22,5 +19,34 @@ class TidysicWindow(QMainWindow):
         self.tree = {}
         self.format = '{title}'
 
+        self._init_layout()
+
+        self._init_menu()
+
+        source_selector = FolderSelect(self, is_input_folder=True)
+        while not source_selector.exec():
+            pass
+        source_dir = source_selector.selectedFiles()[0]
+
+        source = get_audio_files(source_dir)
+
+        ordering_selector = OrderingSelect()
+        while not ordering_selector.exec():
+            pass
+        ordering = ordering_selector.get_ordering()
+
+        root = create_structure(
+            source,
+            ordering,
+            guess=False,
+            dry_run=True
+        )
+
+        self.tree_editor.visualizer.feed_data(root)
+
+    def _init_layout(self):
         self.tree_editor = TreeEditor(self.format)
         self.setCentralWidget(self.tree_editor)
+
+    def _init_menu(self):
+        pass
