bd29a00fd095d92ecb6b9a2a7ea19a518461af4f
g-wizzy <pierre.burki@protonmail.com>
2020-11-25 09:47:39 +0100

    Update the algorithm's tree type

--- tidysic/algorithms.py
+++ tidysic/algorithms.py
@@ -1,5 +1,4 @@
 import os
-from collections import namedtuple
 from typing import List
 
 from .tag import Tag
@@ -13,10 +12,46 @@ from .os_utils import (
 from .logger import log, warning
 
 
-StructureLevel = namedtuple(
-    'StructureLevel',
-    ['ordered', 'unordered']
-)
+class TreeNode(object):
+    '''
+    Node of the tree-like structure used to sort audio files.
+
+    Each TreeNode corresponds to a directory in the output of the program,
+    this means that there is no single root node of the tree, but rather a
+    list of first children.
+    '''
+
+    def __init__(self, name: str, tag: Tag):
+        self._tag = tag
+        self._name = name
+        self._children = []
+
+    @property
+    def tag(self):
+        '''
+        Tag type of the node's ordering level
+
+        For instance, if a node's tag property is Tag.Artists,
+        everyone of its children's name will be artist names
+        '''
+        return self._tag
+
+    @property
+    def name(self):
+        # TODO: Allow for formatting as well as leaves
+        if self._name:
+            return self._name
+        else:
+            return f'Unknown {str(self._tag)}'
+
+    @property
+    def children(self):
+        '''Children nodes, ordered by name'''
+        return self._children
+
+    @children.setter
+    def children(self, value):
+        self._children = value
 
 
 def create_structure(
@@ -24,65 +59,59 @@ def create_structure(
     ordering: List[Tag],
     guess: bool,
     dry_run: bool
-):
+) -> list:
     '''
     Given a list of AudioFiles and an ordering,
-    creates a StructureLevel object.
-
-    It consists of a pair whose first element is a dict whose keys
-    are the values of the tag that were found in the files,
-    and whose values are lists of AudioFiles or further StructureLevel.
-
-    The second element of the pair is a list of all the AudioFiles
-    for which the tag was not found.
+    creates a list of either TreeNode objects or AudioFiles.
     '''
-    ordered = {}
-    unordered = []
 
-    assert(len(ordering) > 0)  # No structure without ordering
+    if not ordering:
+        # No tags for ordering given, we cannot sort the files
+        return audio_files
 
-    order_tag = ordering[0]
+    children = {}
+    order_tag = ordering.pop(0)
 
+    # Sort files into a dictionary
     for file in audio_files:
-
-        tag = file.tags[order_tag]
-        if tag is None:
+        tag_value = file.tags[order_tag]
+        if tag_value is None:
             if order_tag in [Tag.Artist, Tag.Title] and guess:
                 file.guess_tags(dry_run)
 
-                tag = file.tags[order_tag]
+                tag_value = file.tags[order_tag]
 
-                if tag is None:
+                if tag_value is None:
                     log(f'Discarded file: {file}')
             else:
-                # Default behavior is letting the file in the
-                # lowest folder we can.
                 warning(f'''
 File {file.file}
 could not have its {str(order_tag)} tag determined.
-It will stay in the parent folder.\
+It will move into an 'Unknown {str(order_tag)}' directory.\
                 ''')
-                unordered.append(file)
 
-        if tag is not None:
-            if tag not in ordered:
-                ordered[tag] = []
-            ordered[tag].append(file)
+        if tag_value not in children:
+            children[tag_value] = []
+        children[tag_value].append(file)
 
-    if len(ordering) > 1:
-        for tag_value, files in ordered.items():
-            ordered[tag_value] = create_structure(
-                files,
-                ordering[1:],
-                guess,
-                dry_run
-            )
+    # Create nodes from dictionary
+    child_nodes = []
+    for tag_value, files in children.items():
+        new_node = TreeNode(tag_value, order_tag)
+        new_node.children = create_structure(
+            files,
+            ordering,
+            guess,
+            dry_run
+        )
 
-    return StructureLevel(ordered, unordered)
+        child_nodes.append(new_node)
+
+    return child_nodes
 
 
 def move_files(
-    audio_files: StructureLevel,
+    nodes: list,
     dir_target: str,
     format: str,
     dry_run=False,
@@ -90,31 +119,32 @@ def move_files(
     '''
     Moves the given files into a folder hierarchy following
     the given structure.
+
+    Assumes the given TreeNode's children are also TreeNodes
     '''
-    for file in audio_files.unordered:
-        move_file(
-            file.file,
-            file.build_file_name(format),
-            dir_target,
-            dry_run
-        )
+    for child in nodes:
 
-    for tag, content in audio_files.ordered.items():
-        sub_dir_target = create_dir(tag, dir_target, dry_run, False)
+        if isinstance(child, AudioFile):
+            # Leaf of the structure tree
+            move_file(
+                child.file,
+                child.build_file_name(format),
+                dir_target,
+                dry_run,
+                False
+            )
 
-        if isinstance(content, list):  # Leaf of the structure tree
-            for audio_file in content:
-                move_file(
-                    audio_file.file,
-                    audio_file.build_file_name(format),
-                    sub_dir_target,
-                    dry_run,
-                    False
-                )
+        elif isinstance(child, TreeNode):
+            # Recursive step
+            sub_dir_target = create_dir(
+                child.name,
+                dir_target,
+                dry_run,
+                False
+            )
 
-        else:
             move_files(
-                content,
+                child.children,
                 sub_dir_target,
                 format,
                 dry_run
@@ -170,7 +200,7 @@ def organize(
 
     audio_files = get_audio_files(dir_src)
 
-    root = create_structure(
+    root_nodes = create_structure(
         audio_files,
         structure,
         guess,
@@ -179,7 +209,7 @@ def organize(
 
     format = '{title}'
     move_files(
-        root,
+        root_nodes,
         dir_target,
         format,
         dry_run
