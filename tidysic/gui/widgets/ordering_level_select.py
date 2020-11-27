779cd15ab2f1c58b4d81a84d91bfafc21dcc4a1e
g-wizzy <pierre.burki@protonmail.com>
2020-11-27 15:03:24 +0100

    Allow for formatting in ordering_level_select

--- tidysic/gui/widgets/ordering_level_select.py
+++ tidysic/gui/widgets/ordering_level_select.py
@@ -1,31 +1,45 @@
-from PyQt5.QtWidgets import QComboBox
+from PyQt5.QtWidgets import (
+    QWidget,
+    QHBoxLayout,
+    QComboBox,
+    QLineEdit
+)
+
 from PyQt5.QtCore import pyqtSignal
 
 from tidysic.tag import Tag
 
 
-class OrderingLevelSelect(QComboBox):
+class OrderingLevelSelect(QWidget):
 
     changed = pyqtSignal()
 
     def __init__(self, value: Tag, parent, *args, **kwargs):
         super(OrderingLevelSelect, self).__init__(parent, *args, **kwargs)
 
-        self.setInsertPolicy(QComboBox.NoInsert)
-
         self.value = value
 
-        self.addItem("None")
-        for i, tag in enumerate(Tag, 1):
-            self.addItem(str(tag))
-            if tag == value:
-                self.setCurrentIndex(i)
+        layout = QHBoxLayout(self)
+        self.setLayout(layout)
 
-        self.currentIndexChanged.connect(self.onCurrentIndexChanged)
+        self.combo = QComboBox(self)
+        layout.addWidget(self.combo)
+        self.combo.setInsertPolicy(QComboBox.NoInsert)
+        self.combo.addItem("None")
+        for i, tag in enumerate(Tag, 1):
+            self.combo.addItem(str(tag))
+            if tag == value:
+                self.combo.setCurrentIndex(i)
+
+        self.combo.currentIndexChanged.connect(self.onCurrentIndexChanged)
         self.changed.connect(parent.onOrderingLevelsChanged)
 
+        self.format_edit = QLineEdit(self)
+        layout.addWidget(self.format_edit)
+        self._update_format_field()
+
     def onCurrentIndexChanged(self, index):
-        new_text = self.currentText()
+        new_text = self.combo.currentText()
         if new_text == "None":
             self.value = None
         else:
@@ -34,3 +48,13 @@ class OrderingLevelSelect(QComboBox):
                     self.value = tag
 
         self.changed.emit()
+
+    def _update_format_field(self):
+        if self.value is None:
+            self.format_edit.setText('{{track}. }{{title}}')
+        elif self.value == Tag.Album:
+            self.format_edit.setText('{({year}) }{{album}}')
+        else:
+            self.format_edit.setText(
+                f'{{{{{str(self.value).lower()}}}}}'
+            )
