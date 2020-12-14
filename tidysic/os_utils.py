bbb1b9f2e8dbb68d6ecc0e4186fb3f9bb4522946
g-wizzy <pierre.burki@protonmail.com>
2020-12-14 19:04:55 +0100

    Fix verbose and dry_run confusion

--- tidysic/os_utils.py
+++ tidysic/os_utils.py
@@ -73,7 +73,7 @@ def create_dir(
 
     if dry_run or verbose:
         logger.dry_run(f'Create directory {full_path}')
-    if not verbose:
+    if not dry_run:
         os.makedirs(full_path, exist_ok=True)
 
     return full_path
