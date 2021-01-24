c4b1dfcbdaf168ec6fb137cc26f4dd8cdfe021f8
g-wizzy <pierre.burki@protonmail.com>
2021-01-24 22:43:28 +0100

    Remove dev subcommands

--- tidysic/main.py
+++ tidysic/main.py
@@ -1,42 +1,32 @@
-import os
-
 from .argparser import create_parser
 from .logger import log
 from .algorithms import organize
-from .os_utils import lint_folders
 
 
 def run():
     parser = create_parser()
     args = parser.parse_args()
 
+    print("SALUT")
+
     if args.version:
         log('v0.01', prefix='Version')
         exit()
-    elif args.command == 'organize':
-        if args.verbose:
-            log(
-                f'Beginning organizing {args.source} into {args.target}',
-                prefix='verbose',
-                color='green'
-            )
-        organize(
-            args.source,
-            args.target,
-            args.with_album,
-            args.guess,
-            args.dry_run,
-            args.verbose
+
+    if args.verbose:
+        log(
+            f'Beginning organizing {args.source} into {args.target}',
+            prefix='verbose',
+            color='green'
         )
-    elif args.command == 'lint':
-        folders = lint_folders()
-        os.system(f'flake8 {folders[0]} {folders[1]}')
-    elif args.command == 'lintfix':
-        folders = lint_folders()
-        os.system(
-            f'autopep8 --in-place --recursive {folders[0]} {folders[1]}')
-    else:
-        parser.print_usage()
+    organize(
+        args.source,
+        args.target,
+        args.with_album,
+        args.guess,
+        args.dry_run,
+        args.verbose
+    )
 
 
 if __name__ == '__main__':
