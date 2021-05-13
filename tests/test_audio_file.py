9161edf4e8b64f3fe90dda34d88e29133a97f7e3
xiorcale <quentin.vaucher@protonmail.com>
2021-05-13 21:36:44 +0200

    add a few tests

---
+++ tests/test_audio_file.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from tidysic.parser.audio_file import AudioFile
+
+
+path = Path('tests/music/normal/normal.mp3')
+audio_file = AudioFile(path)
+
+
+def test_audio_file_extension():
+    assert audio_file.extension in AudioFile.extensions
+
+
+def test_is_audio_file():
+    assert AudioFile.is_audio_file(path)
+    assert AudioFile.is_audio_file(Path('tests/music/normal')) == False
---
+++ tests/test_tree.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from tidysic.parser.audio_file import AudioFile
+
+from tidysic.parser.tree import Tree
+
+
+def test_tree():
+    tree = Tree(Path('tests/music'))
+
+    def iter(root: Tree):
+        assert isinstance(root, Tree)
+
+        for audio_file in root.audio_files:
+            assert isinstance(audio_file, AudioFile)
+
+        for clutter_file in root.clutter_files:
+            assert isinstance(clutter_file, Path)
+
+        for child in root.children:
+            iter(child)
+
+    iter(tree)
