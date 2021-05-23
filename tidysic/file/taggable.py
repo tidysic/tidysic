9611c8a543af67d791241c2ce0eade225e0c7ae0
g-wizzy <pierre.burki@protonmail.com>
2021-05-23 22:40:05 +0200

    Use ideas from #93

--- tidysic/file/taggable.py
+++ tidysic/file/taggable.py
@@ -21,6 +21,23 @@ class Taggable:
             setattr(self, k, v)
 
     @staticmethod
+    def intersection(taggable: 'Taggable', other: 'Taggable') -> 'Taggable':
+        """
+        Returns the intersection of two `Taggable`. Each field will take either
+        the common value, or keep its default value ("Unknown").
+        """
+        taggables_intersection = Taggable()
+
+        for field in fields(taggable):
+            attr = getattr(taggable, field.name)
+            other_attr = getattr(other, field.name)
+            if attr == other_attr:
+                value = attr
+                setattr(taggables_intersection, field.name, value)
+
+        return taggables_intersection
+
+    @staticmethod
     def get_tag_names() -> tuple[str, ...]:
         return tuple(
             field.name
--- tidysic/parser/tree.py
+++ tidysic/parser/tree.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from typing import Iterator, Callable
+from itertools import chain
+from functools import reduce
 
 from tidysic.file.audio_file import AudioFile
 from tidysic.file.clutter_file import ClutterFile
@@ -20,7 +21,6 @@ class Tree:
         self.clutter_files: set[ClutterFile] = set()
 
         self._parse()
-        self._parse_non_audio()
 
     def _parse(self) -> None:
         """
@@ -30,63 +30,57 @@ class Tree:
         """
         for path in self._root.iterdir():
             if path.is_dir():
-                child = Tree(path)
-                if child.audio_files:
-                    self.children.add(child)
-                else:
-                    self.clutter_files.add(ClutterFile(path))
+                self.children.add(Tree(path))
             elif AudioFile.is_audio_file(path):
                 self.audio_files.add(AudioFile(path))
             else:
                 self.clutter_files.add(ClutterFile(path))
 
-    def _parse_non_audio(self) -> None:
+        self._tag_clutter()
+
+    def _contains_tags(self) -> bool:
+        """
+        Returns True if this node or any of its children has audio files.
+        """
+        return (
+            len(self.audio_files) > 0
+            or
+            any([
+                child._contains_tags()
+                for child in self.children
+            ])
+        )
+
+    def _apply_tags_to_clutter(self, tags: Taggable):
+        """
+        Tags clutter in this node with the given tags.
+
+        Affects its children if they do not contain audio files.
+        """
+        for clutter_file in self.clutter_files:
+            clutter_file.copy_tags_from(tags)
+
+        for child in self.children:
+            if not child._contains_tags():
+                child._apply_tags_to_clutter(tags)
+
+    def _tag_clutter(self):
         """
         Tags non-audio files with the tags common to all audio files in the same
         directory and subdirectories.
         """
-        _ = self._tag_clutter_and_return_common_tags()
+        if self._contains_tags():
+            common_tags = self._find_common_tags()
+            self._apply_tags_to_clutter(common_tags)
 
-    def _tag_clutter_and_return_common_tags(self) -> Taggable:
-        """
-        Tags non-audio files with the tags common to all audio files in the same
-        directory and subdirectories, and returns the set of tags.
-        """
-
-        def all_tagged() -> Iterator[Taggable]:
-            for audio_file in self.audio_files:
-                yield audio_file
-
-            for child in self.children:
-                yield child._tag_clutter_and_return_common_tags()
-
-        for t in all_tagged():
-            print(t)
-
-        common_tags = self._find_common_tags(all_tagged)
-        print(common_tags)
-        print()
-        for clutter_file in self.clutter_files:
-            clutter_file.copy_tags_from(common_tags)
-
-        return common_tags
-
-    @staticmethod
-    def _find_common_tags(
-        taggables: Callable[[], Iterator[Taggable]]
-    ) -> Taggable:
+    def _find_common_tags(self) -> Taggable:
         """
         Finds the common tags shared by the given tagged objects.
         """
-        common_tags = Taggable()
-
-        for tag_name in common_tags.get_tag_names():
-            tag_values = set([
-                getattr(tagged, tag_name)
-                for tagged in taggables()
-            ])
-            if len(tag_values) == 1:
-                tag_value = tag_values.pop()
-                setattr(common_tags, tag_name, tag_value)
-
-        return common_tags
+        children_tags = [
+            child._find_common_tags()
+            for child in self.children
+            if child._contains_tags()
+        ]
+        all_tags = chain(self.audio_files, children_tags)
+        return reduce(Taggable.intersection, all_tags)
