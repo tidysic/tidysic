48c13084d687a7230a16a11d010f5b65fee1917c
g-wizzy <pierre.burki@protonmail.com>
2021-09-17 00:04:49 +0200

    Restore formatted strings

---
+++ tidysic/settings/formatted_string.py
@@ -0,0 +1,70 @@
+import re
+
+from tidysic.file.taggable import Taggable
+
+
+class FormattedString:
+    def __init__(self, raw_string: str):
+        try:
+            self.validate(raw_string)
+        except ValueError as e:
+            raise ValueError(f"Could not create formatted string, {e}")
+
+        self.raw_string = raw_string
+
+    def write(self, taggable: Taggable):
+        """
+        Produces the string built using the tags found in the given taggable
+        """
+        pattern = r"\{(.*?\{(\w+)(:.+?)?\}.*?)\}"
+        matches = re.findall(pattern, self.raw_string)
+
+        return_string = self.raw_string
+
+        substitutions = []
+        for to_substitute, tag_name, format_spec in matches:
+
+            value = getattr(taggable, tag_name, None)
+            if tag_name in Taggable.get_numeric_tag_names() and value:
+                value = int(value)
+            if tag_name in Taggable.get_non_numeric_tag_names() and not value:
+                value = f"Unknown {tag_name}"
+
+            formattable = to_substitute.replace(
+                f"{{{tag_name}{format_spec}}}", f"{{{format_spec}}}"
+            )
+            substitutions.append(
+                (f"{{{to_substitute}}}", formattable.format(value) if value else "")
+            )
+
+        for old, new in substitutions:
+            return_string = return_string.replace(old, new)
+
+        return return_string
+
+    @staticmethod
+    def validate(raw_string: str):
+        """
+        Raises a ValueError if the given string is badly formatted.
+        """
+        bracket_depth = 0
+        current_tag_name = ""
+
+        for i, char in enumerate(raw_string):
+            if char == "{":
+                bracket_depth += 1
+                if bracket_depth > 2:
+                    raise ValueError(f"too many opening brackets (col {i})")
+
+            elif char == "}":
+                bracket_depth -= 1
+                if bracket_depth < 0:
+                    raise ValueError(f"too many closing brackets (col {i})")
+
+                if bracket_depth == 0:
+                    if current_tag_name not in Taggable.get_tag_names():
+                        raise ValueError(f"unknown tag name '{current_tag_name}'")
+                    current_tag_name = ""
+
+            elif bracket_depth == 2:
+                current_tag_name += char
