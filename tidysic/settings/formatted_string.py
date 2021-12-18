93c0cac728b6555116296b9b5f08e83e2dbd9431
g-wizzy <pierre.burki@protonmail.com>
2021-12-18 20:28:35 +0100

    Refactor formatted_string.py

--- tidysic/settings/formatted_string.py
+++ tidysic/settings/formatted_string.py
@@ -1,55 +1,106 @@
 import re
+from abc import ABC, abstractmethod
 
 from tidysic.file.taggable import Taggable
 
 
+class _Unit(ABC):
+
+    @abstractmethod
+    def write(self, taggable: Taggable):
+        pass
+
+
+class _SubstitutableUnit(_Unit):
+
+    def __init__(self, raw_string: str):
+        pattern = r"(\*?)(.*)\{(\w*)(\:.+)?\}(.*)"
+        match = re.fullmatch(pattern, raw_string)
+
+        if match is None:
+            raise ValueError(f"Could not parse formatted string: '{raw_string}'.")
+
+        self.is_required = (match.group(1) == "*")
+        self.text_before = match.group(2)
+        self.tag_name = match.group(3)
+        self.format_spec = match.group(4)
+        self.text_after = match.group(5)
+
+    def write(self, taggable: Taggable):
+        value = self.get_value(taggable)
+        if value is None:
+            if self.is_required:
+                return f"Unknown {self.tag_name}"
+            else:
+                return ""
+        else:
+            if self.format_spec is not None:
+                value = f"{{{self.format_spec}}}".format(value)
+
+            return "".join((
+                self.text_before,
+                value,
+                self.text_after
+            ))
+
+    def get_value(self, taggable: Taggable):
+        value = getattr(taggable, self.tag_name, None)
+        if value and self.tag_name in Taggable.get_numeric_tag_names():
+            if self.tag_name == "tracknumber":
+                match = re.fullmatch(r"(\d+)/\d+", value)
+                if match is not None:
+                    value = match.group(1)
+            value = int(value)
+        return value
+
+
+class _TrivialUnit(_Unit):
+
+    def __init__(self, string: str):
+        self.string = string
+
+    def write(self, taggable: Taggable):
+        return self.string
+
+
 class FormattedString:
+
     def __init__(self, raw_string: str):
         try:
             self.validate(raw_string)
         except ValueError as e:
             raise ValueError(f"Could not create formatted string, {e}")
 
-        self.raw_string = raw_string
+        self._units: list[_Unit] = []
+        self._build_units(raw_string)
+
+    def _build_units(self, raw_string: str):
+        # Substitutable units are found ba looking for exactly two sets of curly
+        # brackets.
+        pattern = r"\{([^\{]*\{[^\{\}]*\}[^\}]*)\}"
+        split = re.split(pattern, raw_string)
+
+        self._units.append(_TrivialUnit(split.pop(0)))
+        try:
+            while split:
+                self._units.append(_SubstitutableUnit(split.pop(0)))
+                self._units.append(_TrivialUnit(split.pop(0)))
+        except ValueError as e:
+            raise ValueError(f"Could not create formatted string, {e}")
 
     def write(self, taggable: Taggable):
         """
         Produces the string built using the tags found in the given taggable.
         """
-        pattern = r"\{((\*)?.*?\{(\w+)(:.+?)?\}.*?)\}"
-        matches = re.findall(pattern, self.raw_string)
-
-        return_string = self.raw_string
-
-        for to_substitute, required_char, tag_name, format_spec in matches:
-
-            required = required_char == "*"
-
-            value = getattr(taggable, tag_name, None)
-            if value and tag_name in Taggable.get_numeric_tag_names():
-                if tag_name == "tracknumber":
-                    match = re.match(r"(\d+)/\d+", value)
-                    if match is not None:
-                        value = match.group(1)
-                value = int(value)
-
-            if not value and required:
-                value = f"Unknown {tag_name}"
-
-            formattable = to_substitute.replace(
-                "%s{%s%s}" % (required_char, tag_name, format_spec),
-                "{%s}" % format_spec,
-            )
-            return_string = return_string.replace(
-                "{%s}" % to_substitute,
-                formattable.format(value) if value else "",
-                1,
-            )
+        return_string = "".join(
+            unit.write(taggable)
+            for unit in self._units
+        )
 
         if len(return_string) == 0:
             raise RuntimeError(
                 "formatted string resulted in empty string. "
-                "Try using the {*{tag}} format to prevent this"
+                "Try using the 'required' marker ({*{tag}}) to prevent this."
             )
 
         return return_string
